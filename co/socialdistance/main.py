c9ead1084c5ce9f038f902a492aaa320428263c5
Katsumi Sakurai <sakurai.katsumi@jp.fujitsu.com>
2021-05-24 13:34:13 +0900

    bugfix etc

--- co/socialdistance/main.py
+++ co/socialdistance/main.py
@@ -2,14 +2,16 @@ import pulp
 from pulp.constants import LpInteger
 import numpy as np
 import math
+import time
 
-x = 11
+x = 9
 y = 10
+bigM = 1000
 
 
 def make_data():
     gap = 1
-    data = [(i * gap, j * gap) for i in range(x) for j in range(y)]
+    data = [(i * gap, j * gap) for j in range(y) for i in range(x)]
     return data
 
 
@@ -21,7 +23,6 @@ def main():
     for i in range(x * y):
         for j in range(x * y):
             dist[i, j] = math.sqrt((data[i][0] - data[j][0])**2 + (data[i][1] - data[j][1])**2)
-    print(dist)
 
     v = [pulp.LpVariable(f'var_{i}_{j}', lowBound=0, upBound=1, cat=LpInteger) for i in range(x) for j in range(y)]
 
@@ -35,7 +36,18 @@ def main():
         for j in range(x * y):
             if i == j:
                 continue
-            problem += ((2 - v[i] - v[j]) * 1000 + dist[i, j]) >= 3.1
+            # problem += ((2 - v[i] - v[j]) * bigM + dist[i, j]) >= 2.2
+            problem += ((2 - v[i] - v[j]) * bigM + dist[i, j]) >= 3.1
+
+    '''
+    # 1列1人
+    for i in range(x):
+         problem += pulp.lpSum(v[j * x + i] for j in range(y)) <= 1
+
+    # 1行1人
+    for j in range(y):
+         problem += pulp.lpSum(v[j * x + i] for i in range(x)) <= 1
+    '''
 
     solver = pulp.PULP_CBC_CMD(msg=0)
     result = problem.solve(solver)
@@ -45,7 +57,7 @@ def main():
     if result == pulp.LpStatusOptimal:
         for j in range(y):
             for i in range(x):
-                if v[i * y + j].value() == 1:
+                if v[j * x + i].value() == 1:
                     print(' o', end='')
                 else:
                     print(' .', end='')
@@ -54,4 +66,7 @@ def main():
 
 
 if __name__ == '__main__':
+    start = time.perf_counter()
     main()
+    end = time.perf_counter()
+    print(f'実行時間: {end - start}')
